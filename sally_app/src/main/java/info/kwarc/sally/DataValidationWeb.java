4d077c30acb679db3e7c73d97db84be3435862ad
alezza <a.lezza@jacobs-university.de>
2014-05-09 18:26:27 +0200

    postInfo commit

--- sally_app/src/main/java/info/kwarc/sally/DataValidationWeb.java
+++ sally_app/src/main/java/info/kwarc/sally/DataValidationWeb.java
@@ -57,13 +57,41 @@ public class DataValidationWeb {
 		data.put("incident_notes", sheetData.getCells().get(3).get(8).getFormula());
 		
 		
+		
+		
 		return te.generateTemplate("validate/validate.ftl", data);
 		//return te.generateTemplate("validate/postInfo.ftl", data);
 		
 	}
 	
 	@POST
-	public String post(@FormParam("incident_id") String incident_id){
-		return incident_id;
+
+	public String post(@QueryParam("file") String file, @QueryParam("sheet") String sheet, @QueryParam("sc") int startCol, @QueryParam("ec") int endCol, @QueryParam("sr") int startRow, @QueryParam("er") int endRow){
+		HashMap<String, Object> data = new HashMap<String, Object>();
+		
+		DocumentInformation docInfo = docManager.getDocumentInformation(file);
+		SpreadsheetDocument spdoc = (SpreadsheetDocument) docInfo.getDocumentModel();
+		Sheet sheetData = spdoc.getConcreteSpreadsheetModel().getSheets().get(sheet);
+
+		log.info(sheetData.getCells().get(4).get(6).getFormula());
+		data.put("incident_id", sheetData.getCells().get(3).get(1).getFormula());
+		data.put("incident_businessapp", sheetData.getCells().get(3).get(4).getFormula());
+		data.put("incident_status", sheetData.getCells().get(3).get(6).getFormula());
+		data.put("incident_priority", sheetData.getCells().get(3).get(7).getFormula());
+		data.put("incident_notes", sheetData.getCells().get(3).get(8).getFormula());
+		data.put("incident_description", sheetData.getCells().get(3).get(9).getFormula());
+		data.put("incident_resolution", sheetData.getCells().get(3).get(10).getFormula());
+		data.put("incident_region", sheetData.getCells().get(3).get(11).getFormula());
+		data.put("incident_servtarget", sheetData.getCells().get(3).get(12).getFormula());
+		data.put("incident_progress", sheetData.getCells().get(3).get(13).getFormula());
+		data.put("incident_opcateg", sheetData.getCells().get(3).get(14).getFormula());
+		data.put("incident_agroup", sheetData.getCells().get(3).get(16).getFormula());
+		
+		return te.generateTemplate("validate/postInfo.ftl", data);
+		
 	}
+	
+	//public String post(@FormParam("incident_id") String incident_id){
+	//	return incident_id;
+	//}
 }
---
+++ sally_app/src/main/resources/sally/validate/postInfo.ftl
@@ -0,0 +1,19 @@
+<textarea>
+
+<datavalid>
+   <incident id="${incident_description}">
+      <buservice>${incident_businessapp}</buservice>
+      <status>Closed${incident_status}</status>
+	  <priority>${incident_priority}</priority>
+	  <notes>${incident_notes}</notes>
+	  <summary>${incident_description}</summary>
+	  <resolution>${incident_resolution}</resolution>
+	  <region>${incident_region}</region>
+	  <starget>${incident_servtarget}</starget>
+	  <iprogress>${incident_progress}</iprogress>
+	  <opcateg>${incident_opcateg}</opcateg>
+	  <agroup>${incident_agroup}</agroup>
+   </incident>
+</datavalid>
+
+</textarea>
